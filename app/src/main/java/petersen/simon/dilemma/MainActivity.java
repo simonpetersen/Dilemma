c78dcb609f9e35d540ddfa7290d8083b2c7840d6
Simon <simon_petersen1994@hotmail.com>
2015-11-24 13:18:12 +0100

    Tilføjet Fragment til MainActivity.

--- app/src/main/java/petersen/simon/dilemma/MainActivity.java
+++ app/src/main/java/petersen/simon/dilemma/MainActivity.java
@@ -11,6 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragmentindhold, new CreateTitleDescImg_frag())
+                    .addToBackStack(null)
+                    .commit();
+        }
     }
 
     @Override
