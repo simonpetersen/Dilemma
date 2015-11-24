ee53f3add1261a1cf521d4a8530a25264680cdb0
Simon <simon_petersen1994@hotmail.com>
2015-11-24 12:04:27 +0100

    Tilføjet klasser til to fragmenter samt tilhørende XML-layouts.

---
+++ app/src/main/java/petersen/simon/dilemma/CreateSeriousCategoryTime_frag.java
@@ -0,0 +1,18 @@
+package petersen.simon.dilemma;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Simon on 24/11/15.
+ */
+public class CreateSeriousCategoryTime_frag extends Fragment {
+
+    public View onCreateView(LayoutInflater i, ViewGroup container, Bundle savedInstanceState) {
+        View v = i.inflate(R.layout.create_serious_frag, container, false);
+        return v;
+    }
+}
---
+++ app/src/main/java/petersen/simon/dilemma/CreateTitleDescImg_frag.java
@@ -0,0 +1,21 @@
+package petersen.simon.dilemma;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Simon on 24/11/15.
+ */
+public class CreateTitleDescImg_frag extends Fragment {
+
+    public View onCreateView(LayoutInflater i, ViewGroup container, Bundle savedInstanceState) {
+        View v = i.inflate(R.layout.create_title_frag, container, false);
+
+        //Opsætning af views.
+
+        return v;
+    }
+}
