8b3d9c962806877e1762953d22a77ec0e56de3bc
Blumen <rasmus_blumensaadt@hotmail.com>
2015-11-25 12:53:43 +0100

    Oprettelse af Mainmenu_frag

---
+++ app/src/main/java/petersen/simon/dilemma/MainMenu_frag.java
@@ -0,0 +1,23 @@
+package petersen.simon.dilemma;
+
+
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Blumen on 25-11-2015.
+ */
+public class MainMenu_frag extends Fragment {
+
+
+    public View onCreateView(LayoutInflater i, ViewGroup container, Bundle SavedInstanceState){
+        View v = i.inflate(R.layout.main_menu_frag, container, false);
+
+        return v;
+    }
+}
