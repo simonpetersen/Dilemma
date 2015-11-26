ed025b32e9df0e63e2a8728da1565acd837b6064
Blumen <rasmus_blumensaadt@hotmail.com>
2015-11-26 15:05:43 +0100

    Implementering af spinner

--- app/src/main/java/petersen/simon/dilemma/CreateSeriousCategoryTime_frag.java
+++ app/src/main/java/petersen/simon/dilemma/CreateSeriousCategoryTime_frag.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 /**
  * Created by Simon on 24/11/15.
@@ -16,6 +17,7 @@ public class CreateSeriousCategoryTime_frag extends Fragment implements AdapterV
 
     Spinner CategorySpinner;
     final String[] Category = new String[] {"Personlig", "Fest", "Hobby", "Begivenhed", "Mode", "Mad", "Karriere", "Andet"};
+    String CategoryChosen = "";
 
     public View onCreateView(LayoutInflater i, ViewGroup container, Bundle savedInstanceState) {
         View v = i.inflate(R.layout.create_serious_frag, container, false);
@@ -27,18 +29,18 @@ public class CreateSeriousCategoryTime_frag extends Fragment implements AdapterV
         adapter.setDropDownViewResource(R.layout.liste_elementer_serioeritetsspinner);
 
         CategorySpinner.setAdapter(adapter);
-        CategorySpinner.setPrompt("Choose a Category.");
+        CategorySpinner.setPrompt("Vælg en kategori.");
         return v;
 
     }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
+    CategoryChosen = Category[position];
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
-
+        Toast.makeText(getActivity(), "Vælg venligst en kategori.", Toast.LENGTH_SHORT).show();
     }
 }
