96baa8dee6845683ca03949c627d3597eb6bd78b
Blumen <rasmus_blumensaadt@hotmail.com>
2015-11-26 20:17:27 +0100

    Main Menu lavet

--- app/src/main/java/petersen/simon/dilemma/MainMenu_frag.java
+++ app/src/main/java/petersen/simon/dilemma/MainMenu_frag.java
@@ -2,18 +2,19 @@ package petersen.simon.dilemma;
 
 
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -21,42 +22,57 @@ import java.util.List;
 /**
  * Created by Blumen on 25-11-2015.
  */
-public class MainMenu_frag extends Fragment implements AdapterView.OnItemClickListener {
+public class MainMenu_frag extends Fragment implements AdapterView.OnItemClickListener, View.OnClickListener {
 
-    List<String> Overskrifter = Arrays.asList("Hjælp til bukser", "Hjælp til kjoler",
+    static List<String> Overskrifter = Arrays.asList("Hjælp til bukser", "Hjælp til kjoler",
             "Skal hunden dø?", "Hvornår skal jeg sige stop?");
-    ArrayList ArrayOverskrifter = new ArrayList(Overskrifter);
-    List<String> Beskrivelse = Arrays.asList("Hjælp mig med hvilke busker jeg skal tage på.",
+    static ArrayList ArrayOverskrifter = new ArrayList(Overskrifter);
+    static List<String> Beskrivelse = Arrays.asList("Hjælp mig med hvilke bukser jeg skal tage på.",
             "Hvilken kjole skal jeg købe til bryllupet?", "Skal min kræftsyge hund aflives?",
             "Min kone bliver ved med at skifte mine ting i lejligheden ud med hendes.. Hvad skal jeg gøre?");
-    ArrayList ArrayBeskrivelse = new ArrayList(Beskrivelse);
+    static ArrayList ArrayBeskrivelse = new ArrayList(Beskrivelse);
+    ListView LV;
+    FloatingActionButton fab;
 
     public View onCreateView(LayoutInflater i, ViewGroup container, Bundle SavedInstanceState){
         View v = i.inflate(R.layout.main_menu_frag, container, false);
-            ArrayAdapter adapter = new ArrayAdapter(getActivity(), R.layout.main_menu_liste,R.id.Overskrift){
+            ArrayAdapter adapter = new ArrayAdapter(getActivity(), R.layout.main_menu_liste,R.id.Overskrift,Overskrifter){
 
                  @Override
                public View getView(int position, View cachedView, ViewGroup parent) {
                    View view = super.getView(position, cachedView, parent);
 
                    TextView beskrivelse = (TextView) view.findViewById(R.id.Beskrivelse);
-                   beskrivelse.setText((String) ArrayBeskrivelse.get(position));
+                   if(((String) ArrayBeskrivelse.get(position)).length() < 35)
+                     beskrivelse.setText((String) ArrayBeskrivelse.get(position));
+                   else
+                    beskrivelse.setText((String) ((String) ArrayBeskrivelse.get(position)).substring(0, 32) + "..." );
 
                    return view;
                }
             };
-        //ListView listView = new ListView(getActivity());
-        ListView listView = (ListView) v.findViewById(R.id.LV);
-        listView.setOnItemClickListener(this);
-        listView.setAdapter(adapter);
 
-        //View v = i.inflate(R.layout.main_menu_frag, listView, false);
+        LV = (ListView) v.findViewById(R.id.LV);
+        LV.setAdapter(adapter);
+        LV.setOnItemClickListener(this);
+
+        fab = (FloatingActionButton) v.findViewById(R.id.fab);
+        fab.setOnClickListener(this);
 
         return v;
     }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(getActivity(), (String) ArrayBeskrivelse.get(position), Toast.LENGTH_LONG).show();
+    }
 
+    @Override
+    public void onClick(View v) {
+        if(v == fab)
+            getFragmentManager().beginTransaction()
+            .replace(R.id.fragmentindhold, new CreateTitleDescImg_frag())
+            .addToBackStack(null)
+            .commit();
     }
 }
