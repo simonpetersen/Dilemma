fd7396cf3af9692ef8a1463d2c8a66c0bb175ffa
Blumen <rasmus_blumensaadt@hotmail.com>
2016-01-04 13:56:34 +0100

    DilemmaList MainMenu ListAdapter

--- app/src/main/java/model/DilemmaList.java
+++ app/src/main/java/model/DilemmaList.java
@@ -32,7 +32,8 @@ public class DilemmaList {
         return overskrifter;
     }
 
-    public Dilemma getDilemmaListe(int posi) { return dilemmaListe.get(posi); }
+    public ArrayList<Dilemma> getDilemmaListe() {return dilemmaListe;}
+    public Dilemma getDilemmaListeElement(int posi) { return dilemmaListe.get(posi); }
     public void addDilemma(Dilemma dilemma) {
         dilemmaListe.add(dilemma);
     }
