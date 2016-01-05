22c9592b5edba873c11e25382bb566cb341626cb
joakimmh <joakim__hansen@hotmail.com>
2016-01-05 13:25:19 +0100

    besvar dilemma xml

--- app/src/main/java/petersen/simon/dilemma/AnswerDilemma_frag.java
+++ app/src/main/java/petersen/simon/dilemma/AnswerDilemma_frag.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.app.Fragment;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import model.Dilemma;
 
@@ -20,6 +21,7 @@ public class AnswerDilemma_frag extends Fragment implements View.OnClickListener
     private Dilemma dilemma;
     private TextView title;
     private Button annuler;
+    private EditText besvarelse;
 
 
     public View onCreateView(LayoutInflater i, ViewGroup container, Bundle SavedInstanceState) {
@@ -28,9 +30,11 @@ public class AnswerDilemma_frag extends Fragment implements View.OnClickListener
         dilemma = MainMenu_frag.dilemmaList.getSelectedDilemma();
         title = (TextView) v.findViewById(R.id.showTitle_answer);
         annuler = (Button) v.findViewById(R.id.buttonTilbage);
+        besvarelse = (EditText) v.findViewById(R.id.editTextBesvarlse);
 
         title.setText(dilemma.getTitle());
         annuler.setOnClickListener(this);
+        besvarelse.setOnClickListener(this);
 
         return v;
     }
@@ -38,6 +42,9 @@ public class AnswerDilemma_frag extends Fragment implements View.OnClickListener
     public void onClick(View v) {
         if (v == annuler) {
             getActivity().getSupportFragmentManager().popBackStack();
+
+        }else if (v == besvarelse){
+
         }
     }
 }
