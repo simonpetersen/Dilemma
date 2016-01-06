1aaa354ba8671acf8c3fcc13a7932459e63a66d8
Blumen <rasmus_blumensaadt@hotmail.com>
2016-01-06 17:33:07 +0100

    Cloudinary start

--- app/src/main/java/diverse/App.java
+++ app/src/main/java/diverse/App.java
@@ -1,6 +1,8 @@
 package diverse;
 
 import android.app.Application;
+import android.content.res.Resources;
+//import android.support.v7.widget.ResourcesWrapper;
 
 import com.cloudinary.Cloudinary;
 import com.firebase.client.Firebase;
@@ -10,6 +12,7 @@ import java.util.Map;
 
 import model.Dilemma;
 import model.DilemmaListe;
+import petersen.simon.dilemma.R;
 
 /**
  * Created by Simon on 05/01/16.
@@ -18,19 +21,22 @@ public class App extends Application {
 
     public static DilemmaListe dilemmaListe;
     public static Dilemma oprettetDilemma;
+    public static Resources resource;
+    public static Cloudinary cloudinary;
 
     @Override
     public void onCreate() {
         super.onCreate();
         dilemmaListe = new DilemmaListe();
         oprettetDilemma = new Dilemma();
+        resource = App.this.getResources();
 
         //Cloudinary-kald her!
         Map config = new HashMap();
-        config.put("cloud_name", "n07t21i7");
-        config.put("api_key", "123456789012345");
-        config.put("api_secret", "abcdeghijklmnopqrstuvwxyz12");
-        Cloudinary cloudinary = new Cloudinary(config);
+        config.put("cloud_name", resource.getString(R.string.cloudinary_cloud_name));
+        config.put("api_key", resource.getString(R.string.cloudinary_api_key));
+        config.put("api_secret", resource.getString(R.string.cloudinary_api_secret));
+        cloudinary = new Cloudinary(config);
 
         //Firebase-kald her!
         Firebase.setAndroidContext(this);
