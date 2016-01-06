ee8d22003038c894decc4bab479ff2a9720b5bba
joakimmh <joakim__hansen@hotmail.com>
2016-01-06 12:56:17 +0100

    Ratingbar

--- app/src/main/java/petersen/simon/dilemma/DilemmaListAdapter.java
+++ app/src/main/java/petersen/simon/dilemma/DilemmaListAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -28,11 +29,12 @@ public class DilemmaListAdapter extends ArrayAdapter {
         //TextView Title = (TextView) view.findViewById(R.id.Title);
         TextView Description = (TextView) view.findViewById(R.id.Description);
         TextView TimeToLive = (TextView) view.findViewById(R.id.TimeToLive);
-        TextView Seriousness = (TextView) view.findViewById(R.id.Seriousness);
+//        TextView Seriousness = (TextView) view.findViewById(R.id.Seriousness);
+        RatingBar Seriousness = (RatingBar) view.findViewById(R.id.Seriousness);
 
         //Title.setText(newDilemmaList.get(position).getTitel());
         TimeToLive.setText(newDilemmaList.get(position).getSvartid() + " minutter tilbage");
-        Seriousness.setText(String.valueOf(newDilemmaList.get(position).getSeriøsitet()));
+        Seriousness.setRating(newDilemmaList.get(position).getSeriøsitet());
 
         if((newDilemmaList.get(position).getBeskrivelse()).length() < 35)
            Description.setText(newDilemmaList.get(position).getBeskrivelse());
