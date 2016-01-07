36218ea585ee6ad5262715d86eed192db2fd4cf5
joakimmh <joakim__hansen@hotmail.com>
2016-01-07 14:19:06 +0100

    switch Kategori (virker ikke inu)

--- app/src/main/java/petersen/simon/dilemma/DilemmaListAdapter.java
+++ app/src/main/java/petersen/simon/dilemma/DilemmaListAdapter.java
@@ -5,6 +5,7 @@ import android.media.Rating;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
@@ -20,6 +21,7 @@ public class DilemmaListAdapter extends ArrayAdapter {
     private ArrayList<Dilemma> newDilemmaList;
     private TextView beskrivelse, tidTilbage;
     private RatingBar seriøsitet;
+    private ImageView kategori;
 
     public DilemmaListAdapter(Context context, int resource, int textViewResourceId, ArrayList<String> titles, ArrayList<Dilemma> DilemmaList) {
         super(context, resource, textViewResourceId, titles);
@@ -33,10 +35,16 @@ public class DilemmaListAdapter extends ArrayAdapter {
         beskrivelse = (TextView) view.findViewById(R.id.Description);
         tidTilbage = (TextView) view.findViewById(R.id.TimeToLive);
         seriøsitet = (RatingBar) view.findViewById(R.id.Seriousness);
-
-        //Title.setText(newDilemmaList.get(position).getTitel());
+        kategori = (ImageView) view.findViewById(R.id.kategori);
         tidTilbage.setText(TimeFormatter.getTidString(newDilemmaList.get(position).getSvartidspunkt()) + " tilbage");
         seriøsitet.setRating(newDilemmaList.get(position).getSeriøsitet());
+
+        switch (newDilemmaList.get(position).getKategori()){
+            case "fest": kategori.setImageResource(R.mipmap.fest);
+                break;
+            case "hobby":
+        }
+        kategori.setImageResource(R.mipmap.);
 
         if((newDilemmaList.get(position).getBeskrivelse()).length() < 35)
            beskrivelse.setText(newDilemmaList.get(position).getBeskrivelse());
