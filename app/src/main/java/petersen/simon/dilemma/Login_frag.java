01f632826b0dc3f357dd2d5a29e25680ef075d60
Blumen <rasmus_blumensaadt@hotmail.com>
2016-01-11 14:16:09 +0100

    login - besvar dilemma

--- app/src/main/java/petersen/simon/dilemma/Login_frag.java
+++ app/src/main/java/petersen/simon/dilemma/Login_frag.java
@@ -39,6 +39,9 @@ public class Login_frag extends Fragment implements View.OnClickListener{
         if(v == loginKnap){
             String loginResultat = App.login(email.getText().toString(), kodeord.getText().toString());
             if (loginResultat != null) Toast.makeText(getActivity(), loginResultat, Toast.LENGTH_SHORT).show();
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.fragmentindhold, new HovedMenu_frag())
+                    .commit();
 
         }
         if(v == nyBruger) {
