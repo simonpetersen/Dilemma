34c0327a25fc9605f94910d4baa1ebb704aafd1e
Sandie <sandieAkkermans@hotmail.com>
2016-01-11 12:32:12 +0100

    Login og Opret bruger er der næsten, mangler firbase delen.

--- app/src/main/java/petersen/simon/dilemma/Login_frag.java
+++ app/src/main/java/petersen/simon/dilemma/Login_frag.java
@@ -7,13 +7,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by Blumen on 05-01-2016.
  */
 public class Login_frag extends Fragment implements View.OnClickListener{
-        EditText Email, Password;
+    EditText Email, Password;
     Button Login;
+    TextView NyBruger;
 
     public View onCreateView(LayoutInflater i, ViewGroup container, Bundle SavedInstanceState){
         View v = i.inflate(R.layout.login_frag, container, false);
@@ -21,13 +24,54 @@ public class Login_frag extends Fragment implements View.OnClickListener{
         Email = (EditText) v.findViewById(R.id.EmailInsert);
         Password = (EditText) v.findViewById(R.id.PasswordInsert);
         Login = (Button) v.findViewById(R.id.LoginButton);
+        NyBruger = (TextView) v.findViewById(R.id.textViewNy);
+
+        Login.setOnClickListener(this);
+        NyBruger.setOnClickListener(this);
 
         return v;
+    }
+
+    private boolean checkInputEmail() {
+        if (Email.getText().toString().equals("")) {
+            return false;
+        }
+        else if (!Email.getText().toString().equals("")) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    private boolean checkInputPassword() {
+        if (Password.getText().toString().equals("")) {
+            return false;
+        }
+        else {
+            return true;
+        }
     }
 
     @Override
     public void onClick(View v) {
         if(v == Login){
+
+            if (checkInputEmail() == false) {
+                Toast.makeText(getActivity(), "Du skal indtaste din e-mail.", Toast.LENGTH_SHORT).show();
+            }
+            else if (checkInputPassword() == false) {
+                Toast.makeText(getActivity(),"Forkert kode, prøv igen." , Toast.LENGTH_SHORT).show();
+            }
+            else {
+                // Kommer noget til firebase.
+            }
+
+        }
+        if(v == NyBruger) {
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.fragmentindhold, new OpretNyBruger_frag())
+                    .commit();
 
         }
     }
--- app/src/main/java/petersen/simon/dilemma/OpretNyBruger_frag.java
+++ app/src/main/java/petersen/simon/dilemma/OpretNyBruger_frag.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by Sandie on 05-01-2016.
@@ -37,10 +38,60 @@ public class OpretNyBruger_frag extends Fragment implements View.OnClickListener
         return  v;
     }
 
+    private boolean checkInputBrugernavn() {
+        if (username.getText().toString().equals("")) {
+            return false;
+        }
+        else if (!username.getText().toString().equals("")) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    private boolean checkInputEmail2() {
+        if (email.getText().toString().equals("")) {
+            return false;
+        }
+        else if (!email.getText().toString().equals("")) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    private boolean checkInputPassword2() {
+        if (password.getText().toString().equals("")) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
     @Override
     public void onClick(View v) {
 
         if(v == create) {
+
+            if(checkInputBrugernavn() == false) {
+                Toast.makeText(getActivity(),"Dette brugernavn eksistere allerede.", Toast.LENGTH_SHORT).show();
+            }
+            else if(checkInputEmail2() == false) {
+                Toast.makeText(getActivity(),"Denne e-mail er allerede i brug.", Toast.LENGTH_SHORT).show();
+            }
+            else if(checkInputPassword2() == false) {
+                Toast.makeText(getActivity(),"Skriv et kode.", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                // Kommer noget til firebase.
+                getFragmentManager().beginTransaction()
+                        .replace(R.id.fragmentindhold, new HovedMenu_frag())
+                        .commit();
+            }
+
 
         }
 
