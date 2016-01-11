3f80b0ed127f05871a93655e53d1bd171905a96d
Sandie <sandieAkkermans@hotmail.com>
2016-01-11 13:55:44 +0100

    Sender inden de bliver slettet igen!

---
+++ app/src/main/java/petersen/simon/dilemma/Besvarelser.java
@@ -0,0 +1,7 @@
+package petersen.simon.dilemma;
+
+/**
+ * Created by Sandie on 11-01-2016.
+ */
+public class Besvarelser {
+}
---
+++ app/src/main/java/petersen/simon/dilemma/VisStatistik_frag.java
@@ -0,0 +1,14 @@
+package petersen.simon.dilemma;
+
+import android.support.v4.app.Fragment;
+import android.view.View;
+
+/**
+ * Created by Sandie on 11-01-2016.
+ */
+public class VisStatistik_frag extends Fragment implements View.OnClickListener {
+    @Override
+    public void onClick(View v) {
+
+    }
+}
