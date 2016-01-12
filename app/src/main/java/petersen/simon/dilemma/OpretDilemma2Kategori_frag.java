79a77b2d7076b7289db7747c17e9153677b3c12b
Sandie <sandieAkkermans@hotmail.com>
2016-01-12 10:45:38 +0100

    ..

--- app/src/main/java/petersen/simon/dilemma/OpretDilemma2Kategori_frag.java
+++ app/src/main/java/petersen/simon/dilemma/OpretDilemma2Kategori_frag.java
@@ -2,19 +2,14 @@ package petersen.simon.dilemma;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.Spinner;
-import android.widget.TableLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -99,14 +94,19 @@ public class OpretDilemma2Kategori_frag extends Fragment
     public void onClick(View v) {
         if(v == Ok) {
 
+
+
             if(CategoryChosen.equals("Vælg kategori")) {
                 Toast.makeText(getActivity(), "Du mangler at vælge kategori.", Toast.LENGTH_SHORT).show();
+                Ok.setBackgroundDrawable(getResources().getDrawable(R.drawable.drawer_shadow));
             }
             else if(Seriousness.getProgress() == 0) {
                 Toast.makeText(getActivity(), "Du mangler at vælge seriøsitetsgrad.", Toast.LENGTH_SHORT).show();
+                Ok.setBackgroundDrawable(getResources().getDrawable(R.drawable.drawer_shadow));
             }
             else if(Timer.getProgress() == 0) {
                 Toast.makeText(getActivity(), "Du mangler at vælge tiden.", Toast.LENGTH_SHORT).show();
+                Ok.setBackgroundDrawable(getResources().getDrawable(R.drawable.drawer_shadow));
             }
 
             else {
