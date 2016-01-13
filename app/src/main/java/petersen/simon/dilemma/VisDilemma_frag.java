133d5378790af1f792896b689c0ab6b2c464c9b1
Simon <simon_petersen1994@hotmail.com>
2016-01-13 13:23:08 +0100

    Beskrivelse er under billeder i visDilemma.

--- app/src/main/java/petersen/simon/dilemma/VisDilemma_frag.java
+++ app/src/main/java/petersen/simon/dilemma/VisDilemma_frag.java
@@ -8,10 +8,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewManager;
 import android.widget.Button;
+import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.Date;
 
@@ -22,8 +27,9 @@ import model.Dilemma;
 public class VisDilemma_frag extends Fragment implements View.OnClickListener {
 
     private Dilemma dilemma;
-    private TextView title, beskrivelse, seriøsitet, udløb;
+    private TextView title, beskrivelseTekstView, beskrivelseInfoTekst, seriøsitet, udløb;
     private Button besvar, slet;
+    private HorizontalScrollView galleriContainer;
     private LinearLayout galleri;
 
     public View onCreateView(LayoutInflater i, ViewGroup container, Bundle savedInstanceState) {
@@ -33,22 +39,29 @@ public class VisDilemma_frag extends Fragment implements View.OnClickListener {
         dilemma = App.valgtDilemma;
 
         title = (TextView) v.findViewById(R.id.showTitle);
-        beskrivelse = (TextView) v.findViewById(R.id.showDescription);
+        beskrivelseTekstView = (TextView) v.findViewById(R.id.beskrivelseTekst);
+        beskrivelseInfoTekst = (TextView) v.findViewById(R.id.beskrivelse);
         seriøsitet = (TextView) v.findViewById(R.id.showSeriøsitet);
         udløb = (TextView) v.findViewById(R.id.showUdløb);
         besvar = (Button) v.findViewById(R.id.besvarButton);
         slet = (Button) v.findViewById(R.id.slet);
         galleri = (LinearLayout) v.findViewById(R.id.galleri);
+        galleriContainer = (HorizontalScrollView) v.findViewById(R.id.horizontalScrollView);
 
-        //Indæst billeder i Galleri
-        if (dilemma.getBilledeUrl() != null) {
-            for (String s : dilemma.getBilledeUrl()) {
-                ImageView iv = new ImageView(getActivity());
-                iv.setAdjustViewBounds(true);
-                App.downloadBillede(s, iv);
-                galleri.addView(iv);
-            }
+
+        if (dilemma.getBeskrivelse().equals("")) beskrivelseInfoTekst.setVisibility(View.INVISIBLE);
+
+        galleriContainer.getLayoutParams().height = 0;
+        v.requestLayout();
+
+            //Indæst billeder i Galleri
+        for (String s : dilemma.getBilledeUrl()) {
+            ImageView iv = new ImageView(getActivity());
+            iv.setAdjustViewBounds(true);
+            App.downloadBillede(s, iv);
+            galleri.addView(iv);
         }
+
         if(App.userID == null)
             besvar.setVisibility(View.INVISIBLE);
 
@@ -56,7 +69,7 @@ public class VisDilemma_frag extends Fragment implements View.OnClickListener {
             slet.setVisibility(View.INVISIBLE);
 
         title.setText(dilemma.getTitel());
-        beskrivelse.setText(dilemma.getBeskrivelse());
+        beskrivelseTekstView.setText(dilemma.getBeskrivelse());
         seriøsitet.setText(String.valueOf(dilemma.getSeriøsitet()));
 
         new CountDownTimer(dilemma.getSvartidspunkt() - new Date().getTime(), 1000) {
