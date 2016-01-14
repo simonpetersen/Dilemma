afde0d739aa76e29c5431c5316ece5194d4bc192
Simon <simon_petersen1994@hotmail.com>
2016-01-14 21:47:01 +0100

    Søjle-diagram bliver vist i VisStatistik.

--- app/src/main/java/petersen/simon/dilemma/VisStatistik_frag.java
+++ app/src/main/java/petersen/simon/dilemma/VisStatistik_frag.java
@@ -1,39 +1,61 @@
 package petersen.simon.dilemma;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.BarGraphSeries;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
+
+import diverse.App;
+import model.BesvarelseListe;
 
 /**
  * Created by Sandie on 11-01-2016.
  */
-public class VisStatistik_frag extends Fragment implements View.OnClickListener {
+public class VisStatistik_frag extends Fragment{
 
     GraphView graph;
+    TextView titelView;
+    BesvarelseListe besvarelseListe;
 
     @Override
     public View onCreateView(LayoutInflater i, ViewGroup container, Bundle SavedIntanceState) {
         View v = i.inflate(R.layout.vis_statistik_frag, container, false);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[]{
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3),
-                new DataPoint(3, 2),
-                new DataPoint(4, 6)
-        });
-        graph.addSeries(series);
+        titelView = (TextView) v.findViewById(R.id.titelView);
+        titelView.setText(App.valgtDilemma.getTitel());
+
+        graph = (GraphView) v.findViewById(R.id.graph);
+        besvarelseListe = App.getBesvarelsesListe(App.valgtDilemma.getDilemmaID());
+        int[] colors = {Color.parseColor("#FF0001D1"), Color.parseColor("#FFFF4743"), Color.parseColor("#FFFF7600"), Color.DKGRAY};
+
+        DataPoint[] dataPoints = new DataPoint[1];
+        BarGraphSeries<DataPoint> series;
+        for (int n=0; n < besvarelseListe.getSvar().length; n++) {
+            dataPoints[0] = new DataPoint(n+1, besvarelseListe.getSvar()[n]);
+            series = new BarGraphSeries<>(dataPoints);
+            series.setTitle(App.valgtDilemma.getSvarmuligheder().get(n));
+            graph.addSeries(series);
+            series.setDrawValuesOnTop(true);
+            series.setValuesOnTopColor(Color.BLACK);
+            series.setColor(colors[n]);
+        }
+
+        graph.getGridLabelRenderer().setHorizontalLabelsVisible(false);
+        graph.getViewport().setScalable(true);
+
+        graph.getViewport().setXAxisBoundsManual(true);
+        graph.getViewport().setMinX(0.0);
+        graph.getViewport().setMaxX(besvarelseListe.getSvar().length+1);
+
+        graph.getLegendRenderer().setVisible(true);
 
         return v;
-    }
-
-    @Override
-    public void onClick(View v) {
-
     }
 }
