a609a0df31fb14fb16b1e695e21ea493c74b1b5e
Simon <simon_petersen1994@hotmail.com>
2016-01-14 14:25:34 +0100

    Ændret navn på app til g41 Dilemma.

--- app/src/main/java/petersen/simon/dilemma/VisStatistik_frag.java
+++ app/src/main/java/petersen/simon/dilemma/VisStatistik_frag.java
@@ -6,11 +6,31 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 /**
  * Created by Sandie on 11-01-2016.
  */
 public class VisStatistik_frag extends Fragment implements View.OnClickListener {
 
+    GraphView graph;
+
+    @Override
+    public View onCreateView(LayoutInflater i, ViewGroup container, Bundle SavedIntanceState) {
+        View v = i.inflate(R.layout.vis_statistik_frag, container, false);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[]{
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
+
+        return v;
+    }
 
     @Override
     public void onClick(View v) {
